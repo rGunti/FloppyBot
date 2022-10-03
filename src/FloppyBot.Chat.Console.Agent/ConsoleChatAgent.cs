a874deef1805b368d19ea05a0b20268c07ef20b6
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-10-03 22:19:06 +0200

    implemented a console agent for local testing

---
+++ src/FloppyBot.Chat.Console.Agent/ConsoleChatAgent.cs
@@ -0,0 +1,60 @@
+using FloppyBot.Base.Configuration;
+using FloppyBot.Chat.Entities;
+using FloppyBot.Communication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FloppyBot.Chat.Console.Agent;
+
+internal class ConsoleChatAgent : BackgroundService
+{
+    private readonly ILogger<ConsoleChatAgent> _logger;
+    private readonly INotificationSender _sender;
+    private readonly ConsoleChatInterface _chatInterface;
+
+    public ConsoleChatAgent(
+        ILogger<ConsoleChatAgent> logger,
+        INotificationSenderFactory senderFactory,
+        IConfiguration configuration,
+        ConsoleChatInterface chatInterface)
+    {
+        _logger = logger;
+        _chatInterface = chatInterface;
+        _chatInterface.MessageReceived += OnMessageReceived;
+
+        _sender = senderFactory.GetNewSender(
+            configuration.GetParsedConnectionString("MessageOutput"));
+    }
+
+    private void OnMessageReceived(IChatInterface sourceInterface, ChatMessage chatMessage)
+    {
+        _logger.LogDebug("Sending message {@Message}", chatMessage);
+        _sender.Send(chatMessage);
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogWarning(
+            "Welcome to the FloppyBot Console Agent! " +
+            "Please note that this is a development tool and not meant to be run in production environments!");
+        _logger.LogInformation("Starting Console Agent ...");
+        _chatInterface.Connect();
+        return base.StartAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(500, stoppingToken);
+        }
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Shutting down Console Agent (you might need to press [ENTER]) ...");
+        _chatInterface.Disconnect();
+        return base.StopAsync(cancellationToken);
+    }
+}
---
+++ src/FloppyBot.Chat.Console.Agent/ConsoleChatInterface.cs
@@ -0,0 +1,103 @@
+using FloppyBot.Chat.Entities;
+using FloppyBot.Chat.Entities.Identifiers;
+using Microsoft.Extensions.Logging;
+
+namespace FloppyBot.Chat.Console.Agent;
+
+internal class ConsoleChatInterface : IChatInterface
+{
+    private const string IF_NAME = "Console";
+
+    private readonly ILogger<ConsoleChatInterface> _logger;
+    private readonly ConsoleAgentUserConfiguration _userConfig;
+
+    private readonly Thread _consoleInputThread;
+
+    public ConsoleChatInterface(
+        ILogger<ConsoleChatInterface> logger,
+        ConsoleAgentUserConfiguration userConfig)
+    {
+        _userConfig = userConfig;
+        _logger = logger;
+
+        _consoleInputThread = new Thread(ConsoleLoop);
+    }
+
+    private bool _active;
+
+    public string Name => IF_NAME;
+    public ChatInterfaceFeatures SupportedFeatures => ChatInterfaceFeatures.None;
+
+    public void Connect()
+    {
+        _logger.LogInformation("Configured console user as {@UserConfig}", _userConfig);
+        _consoleInputThread.Start();
+
+        var task = new Task(ConsoleLoop);
+        task.Start();
+    }
+
+    private void ConsoleLoop()
+    {
+        _logger.LogInformation("Console ready! Enter a message and send using [ENTER]");
+        _active = true;
+
+        while (_active)
+        {
+            string? chatMessage = System.Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(chatMessage))
+            {
+                MessageReceived?.Invoke(
+                    this,
+                    CreateMessage(chatMessage));
+            }
+        }
+
+        _logger.LogInformation("Console is disconnected");
+    }
+
+    private ChatMessage CreateMessage(string message)
+    {
+        return new ChatMessage(
+            new ChatMessageIdentifier(
+                IF_NAME,
+                "Main",
+                Guid.NewGuid().ToString()),
+            new ChatUser(
+                new ChannelIdentifier(
+                    IF_NAME,
+                    _userConfig.Username),
+                _userConfig.Username,
+                _userConfig.PrivilegeLevel),
+            SharedEventTypes.CHAT_MESSAGE,
+            message);
+    } 
+
+    public void Disconnect()
+    {
+        _logger.LogInformation("Disconnecting ...");
+        _active = false;
+
+        _consoleInputThread.Join(TimeSpan.FromSeconds(1));
+        _logger.LogInformation("Disconnected");
+    }
+
+    public void SendMessage(string message)
+    {
+        _logger.LogInformation("Message: {Message}", message);
+    }
+
+    public void SendMessage(ChannelIdentifier channel, string message)
+    {
+        SendMessage(message);
+    }
+
+    public event ChatMessageReceivedDelegate? MessageReceived;
+    
+    public void Dispose()
+    {
+        if (_consoleInputThread.IsAlive)
+        {
+        }
+    }
+}
