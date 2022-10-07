63841860b08c29216fdfb03f687d4e75d22248f4
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-10-07 22:42:32 +0200

    setup logger so logging in unit tests is possible

--- src/FloppyBot.Base.Logging/SerilogSetup.cs
+++ src/FloppyBot.Base.Logging/SerilogSetup.cs
@@ -1,27 +1,42 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Configuration;
 
 namespace FloppyBot.Base.Logging;
 
 public static class SerilogSetup
 {
+    private const string OUTPUT_TEMPLATE =
+        "{Timestamp:HH:mm:ss.fff} | {SourceContext,-75} | {Level:u3} | {Message:lj}{NewLine}{Exception}";
+
     public static IHostBuilder SetupSerilog(this IHostBuilder hostBuilder)
     {
         return hostBuilder
             .UseSerilog((ctx, lc) => lc.ConfigureSerilog(ctx.Configuration));
     }
 
+    internal static LoggerConfiguration ConfigureCommonSerilogSettings(
+        this LoggerConfiguration loggerConfig)
+    {
+        return loggerConfig
+            .Enrich.FromLogContext()
+            .Enrich.WithThreadId();
+    }
+
+    internal static LoggerConfiguration CommonConsoleOutput(this LoggerSinkConfiguration sinkConfig)
+    {
+        return sinkConfig
+            .Console(outputTemplate: OUTPUT_TEMPLATE);
+    }
+
     public static LoggerConfiguration ConfigureSerilog(this LoggerConfiguration loggerConfig, IConfiguration hostConfig)
     {
         return loggerConfig
             // - Default Log Configuration
-            .Enrich.FromLogContext()
-            .Enrich.WithThreadId()
+            .ConfigureCommonSerilogSettings()
             .WriteTo.Async(s => s
-                .Console(
-                    outputTemplate:
-                    "{Timestamp:HH:mm:ss.fff} | {SourceContext,-75} | {Level:u3} | {Message:lj}{NewLine}{Exception}"))
+                .CommonConsoleOutput())
 #if DEBUG
             .MinimumLevel.Verbose()
 #else
@@ -29,5 +44,13 @@ public static class SerilogSetup
 #endif
             // - Configurable via JSON file
             .ReadFrom.Configuration(hostConfig);
+    }
+
+    public static LoggerConfiguration ConfigureSerilogForTesting(this LoggerConfiguration loggerConfig)
+    {
+        return loggerConfig
+            .ConfigureCommonSerilogSettings()
+            .WriteTo.CommonConsoleOutput()
+            .MinimumLevel.Verbose();
     }
 }
--- src/FloppyBot.Base.Testing/LoggingUtils.cs
+++ src/FloppyBot.Base.Testing/LoggingUtils.cs
@@ -1,12 +1,32 @@
+using FloppyBot.Base.Logging;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using Serilog;
 
 namespace FloppyBot.Base.Testing;
 
 public static class LoggingUtils
 {
+    private static readonly Lazy<ILoggerFactory> LoggerFactory = new(InitRealLogger);
+    private static readonly Lazy<ILoggerFactory> NullLoggerFac = new(NullLoggerFactory.Instance);
+
+    public static bool UseRealLogger { get; set; } = true;
+
+    private static ILoggerFactory InitRealLogger()
+    {
+        var serilogLogger = new LoggerConfiguration()
+            .ConfigureSerilogForTesting()
+            .CreateLogger();
+
+        return Microsoft.Extensions.Logging.LoggerFactory.Create(loggingBuilder =>
+        {
+            loggingBuilder
+                .AddSerilog(serilogLogger);
+        });
+    }
+
     public static ILogger<T> GetLogger<T>()
     {
-        return NullLogger<T>.Instance;
+        return (UseRealLogger ? LoggerFactory : NullLoggerFac).Value.CreateLogger<T>();
     }
 }
