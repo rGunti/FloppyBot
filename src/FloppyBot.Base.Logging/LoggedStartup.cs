827567d51fe8bfbf14e625493034d7ca3d4b34a9
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-10-25 21:19:08 +0200

    implemented version system

---
+++ src/FloppyBot.Base.Logging/LoggedStartup.cs
@@ -0,0 +1,26 @@
+﻿using FloppyBot.Version;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FloppyBot.Base.Logging;
+
+public static class LoggedStartup
+{
+    public static Task LogAndRun(
+        this IHost host)
+    {
+        host.Services.GetRequiredService<ILogger<Run>>()
+            .LogInformation(
+                "Starting up application {AppName} {ServiceName} {ServiceVersion}",
+                AboutThisApp.Name,
+                AboutThisApp.ServiceName,
+                AboutThisApp.Version);
+        return host
+            .RunAsync();
+    }
+
+    private class Run
+    {
+    }
+}
---
+++ src/FloppyBot.Version/AboutThisApp.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+namespace FloppyBot.Version;
+
+public static class AboutThisApp
+{
+    static AboutThisApp()
+    {
+        var assembly = Assembly.GetEntryAssembly()!;
+        Version = assembly.GetProductVersion()!;
+        ServiceName = assembly.GetProductName()!;
+    }
+
+    public static string Name => "FloppyBot";
+    public static string ServiceName { get; }
+    public static string Version { get; }
+
+    private static string? GetProductName(this Assembly assembly)
+        => assembly.GetAssemblyAttribute<AssemblyProductAttribute>(v => v.Product);
+
+    private static string? GetProductVersion(this Assembly assembly)
+        => assembly.GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(v => v.InformationalVersion);
+
+    private static string? GetAssemblyAttribute<T>(
+        this Assembly assembly,
+        Func<T, string> valueSelectorFn) where T : Attribute
+    {
+        return assembly
+            .GetCustomAttributes<T>()
+            .Select(valueSelectorFn)
+            .FirstOrDefault();
+    }
+}
