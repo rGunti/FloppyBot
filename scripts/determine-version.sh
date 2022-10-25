6d3ecf68d59c06abeb0ebeb92b37bf5b81929efe
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-10-26 00:02:21 +0200

    rewrote logic to define version

---
+++ scripts/determine-version.sh
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env bash
+source /semver.sh
+
+local MAJOR=0  
+local MINOR=0  
+local PATCH=0  
+local SPECIAL=""
+
+semverParseInto "$(cat /app-version)" MAJOR MINOR PATCH SPECIAL
+
+echo MAJOR=${MAJOR}
+echo MINOR=${MINOR}
+echo PATCH=${PATCH}
+echo SPECIAL=${SPECIAL}
+
+echo "${MAJOR}.${MINOR}.${PATCH}" > /app-short-version
