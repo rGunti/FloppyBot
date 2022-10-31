c153617b9273dce6c542ef707ef09d9c051111c2
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-10-31 23:08:59 +0100

    refactoring

--- src/FloppyBot.Base.Storage.LiteDb/Registration.cs
+++ src/FloppyBot.Base.Storage.LiteDb/Registration.cs
@@ -24,16 +24,3 @@ public static class Registration
             .AddStorageImplementation<LiteDbRepositoryFactory>();
     }
 }
-
-public class LiteDbInstanceFactory
-{
-    public const string IN_MEMORY = ":inmem:";
-    private Stream ConstructMemoryStream() => new MemoryStream();
-
-    public ILiteDatabase ConstructDatabaseInstance(string connectionString)
-    {
-        return connectionString == IN_MEMORY ? ConstructMemoryDbInstance() : new LiteDatabase(connectionString);
-    }
-
-    public ILiteDatabase ConstructMemoryDbInstance() => new LiteDatabase(ConstructMemoryStream());
-}
