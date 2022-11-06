11909d23f8ea0525ee729c301fa93ea86a67468a
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-11-06 20:36:46 +0100

    added missing file

--- src/FloppyBot.WebApi.Base/Exceptions/HttpStatusCodeExceptionExtensions.cs
+++ src/FloppyBot.WebApi.Base/Exceptions/HttpStatusCodeExceptionExtensions.cs
@@ -9,4 +9,10 @@ public static class HttpStatusCodeExceptionExtensions
         return new RouteNotImplementedException(
             $"Route \"{controller.Request.Method} {controller.Request.Path}\" is not implemented");
     }
+
+    public static HttpStatusCodeException Obsolete(this ControllerBase controller)
+    {
+        return new BadRequestException(
+            $"Route \"{controller.Request.Method} {controller.Request.Path}\" is marked obsolete and will not be implemented");
+    }
 }
