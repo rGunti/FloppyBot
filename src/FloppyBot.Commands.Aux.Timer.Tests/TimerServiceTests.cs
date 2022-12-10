8080caaef8917cec69d56c93ed05002bdfc21210
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-12-10 19:19:59 +0100

    can now query expired timers

---
+++ src/FloppyBot.Commands.Aux.Timer.Tests/TimerServiceTests.cs
@@ -0,0 +1,121 @@
+using FloppyBot.Base.Clock;
+using FloppyBot.Base.Storage;
+using FloppyBot.Base.Storage.LiteDb;
+using FloppyBot.Commands.Aux.Timer.Storage;
+using FloppyBot.Commands.Aux.Timer.Storage.Entities;
+
+namespace FloppyBot.Commands.Aux.Timer.Tests;
+
+[TestClass]
+public class TimerServiceTests
+{
+    private static readonly DateTimeOffset RefTime = DateTimeOffset.Parse("2022-12-12T12:00:00Z");
+    private readonly IRepository<TimerRecord> _repository;
+    private readonly LiteDbRepositoryFactory _repositoryFactory;
+
+    private readonly TimerService _service;
+    private readonly FixedTimeProvider _timeProvider;
+
+    public TimerServiceTests()
+    {
+        _timeProvider = new FixedTimeProvider(RefTime);
+        _repositoryFactory = LiteDbRepositoryFactory.CreateMemoryInstance();
+        _repository = _repositoryFactory.GetRepository<TimerRecord>();
+        _service = new TimerService(
+            _repositoryFactory,
+            _timeProvider);
+    }
+
+    [TestMethod]
+    public void CreateNewRecord()
+    {
+        _service.CreateTimer(
+            "Mock/Channel/Message",
+            "Mock/User",
+            5.Minutes(),
+            "Hello World");
+
+        Assert.AreEqual(
+            new TimerRecord(
+                "test",
+                "Mock/Channel/Message",
+                "Hello World",
+                DateTimeOffset.Parse("2022-12-12T12:05:00Z"),
+                RefTime,
+                "Mock/User"),
+            _repository.GetAll()
+                .Select(t => t.WithId("test"))
+                .Single());
+    }
+
+    [TestMethod]
+    public void FetchExpiredTimers()
+    {
+        var expectedMessage = new TimerRecord(
+            "test",
+            "Mock/Channel/Message1",
+            "Expected",
+            DateTimeOffset.Parse("2022-12-12T12:05:00Z"),
+            RefTime,
+            "Mock/User");
+        var unexpectedMessage = new TimerRecord(
+            "not_test",
+            "Mock/Channel/Message2",
+            "Unexpected",
+            DateTimeOffset.Parse("2022-12-13T12:34:56Z"),
+            RefTime,
+            "Mock/AnotherUser");
+        _repository.Insert(expectedMessage);
+        _repository.Insert(unexpectedMessage);
+
+        _timeProvider.AdvanceTimeBy(5.Minutes());
+
+        TimerRecord[] expiredMessages = _service.GetExpiredTimers(false)
+            .ToArray();
+
+        CollectionAssert.AreEquivalent(
+            new[]
+            {
+                expectedMessage
+            },
+            expiredMessages);
+
+        Assert.IsNotNull(_repository.GetById("test"), "Timer has been deleted unexpectedly");
+    }
+
+    [TestMethod]
+    public void FetchAndDeleteExpiredTimers()
+    {
+        var expectedMessage = new TimerRecord(
+            "test",
+            "Mock/Channel/Message1",
+            "Expected",
+            DateTimeOffset.Parse("2022-12-12T12:05:00Z"),
+            RefTime,
+            "Mock/User");
+        var unexpectedMessage = new TimerRecord(
+            "not_test",
+            "Mock/Channel/Message2",
+            "Unexpected",
+            DateTimeOffset.Parse("2022-12-13T12:34:56Z"),
+            RefTime,
+            "Mock/AnotherUser");
+        _repository.Insert(expectedMessage);
+        _repository.Insert(unexpectedMessage);
+
+        _timeProvider.AdvanceTimeBy(5.Minutes());
+
+        TimerRecord[] expiredMessages = _service.GetExpiredTimers(true)
+            .ToArray();
+
+        CollectionAssert.AreEquivalent(
+            new[]
+            {
+                expectedMessage
+            },
+            expiredMessages);
+
+        Assert.IsNull(_repository.GetById("test"), "Timer has not been deleted unexpectedly");
+    }
+}
+
--- src/FloppyBot.Commands.Aux.Timer/Storage/ITimerService.cs
+++ src/FloppyBot.Commands.Aux.Timer/Storage/ITimerService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Immutable;
 using FloppyBot.Base.Clock;
 using FloppyBot.Base.Storage;
 using FloppyBot.Commands.Aux.Timer.Storage.Entities;
@@ -11,6 +12,10 @@ public interface ITimerService
         string author,
         TimeSpan timerLength,
         string timerMessage);
+
+    IEnumerable<TimerRecord> GetExpiredTimers(bool delete);
+    void DeleteTimers(IEnumerable<TimerRecord> timers);
+    void DeleteTimer(TimerRecord timer);
 }
 
 public class TimerService : ITimerService
@@ -36,5 +41,31 @@ public class TimerService : ITimerService
             author);
         _repository.Insert(timer);
     }
+
+    public IEnumerable<TimerRecord> GetExpiredTimers(bool delete)
+    {
+        DateTimeOffset now = _timeProvider.GetCurrentUtcTime();
+        ImmutableArray<TimerRecord> timers = _repository.GetAll()
+            .Where(t => t.TargetTime <= now)
+            .ToImmutableArray();
+
+        if (timers.Any() && delete)
+        {
+            DeleteTimers(timers);
+        }
+
+        return timers;
+    }
+
+    public void DeleteTimers(IEnumerable<TimerRecord> timers)
+    {
+        _repository.Delete(timers);
+    }
+
+    public void DeleteTimer(TimerRecord timer)
+    {
+        _repository.Delete(timer);
+    }
 }
+
 
