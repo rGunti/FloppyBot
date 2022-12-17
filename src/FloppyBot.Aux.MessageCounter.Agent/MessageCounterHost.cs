e9d09051eb23fdfe816b0fcd779a895bbaa48419
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2022-12-17 22:14:24 +0100

    implemented new agent for counting received messages by channel

---
+++ src/FloppyBot.Aux.MessageCounter.Agent/MessageCounterHost.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FloppyBot.Aux.MessageCounter.Agent;
+
+public class MessageCounterHost : BackgroundService
+{
+    private readonly ILogger<MessageCounterHost> _logger;
+    private readonly Core.MessageCounter _messageCounter;
+
+    public MessageCounterHost(ILogger<MessageCounterHost> logger, Core.MessageCounter messageCounter)
+    {
+        _logger = logger;
+        _messageCounter = messageCounter;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Starting up Message Counter Agent ...");
+        _messageCounter.Start();
+
+        _logger.LogInformation("Awaiting new messages to count");
+        return Task.CompletedTask;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Shutting down Message Counter Agent ...");
+        _messageCounter.Stop();
+        return base.StopAsync(cancellationToken);
+    }
+}
+
+
---
+++ src/FloppyBot.Aux.MessageCounter.Core/MessageOccurrenceService.cs
@@ -0,0 +1,41 @@
+﻿using FloppyBot.Aux.MessageCounter.Core.Entities;
+using FloppyBot.Base.Clock;
+using FloppyBot.Base.Storage;
+using FloppyBot.Chat.Entities;
+using FloppyBot.Chat.Entities.Identifiers;
+
+namespace FloppyBot.Aux.MessageCounter.Core;
+
+public class MessageOccurrenceService : IMessageOccurrenceService
+{
+    private readonly IRepository<MessageOccurrence> _repository;
+    private readonly ITimeProvider _timeProvider;
+
+    public MessageOccurrenceService(
+        IRepositoryFactory repositoryFactory,
+        ITimeProvider timeProvider)
+    {
+        _timeProvider = timeProvider;
+        _repository = repositoryFactory.GetRepository<MessageOccurrence>();
+    }
+
+    public void StoreMessage(ChatMessage chatMessage)
+    {
+        _repository.Insert(new MessageOccurrence(
+            chatMessage.Identifier,
+            chatMessage.Identifier.GetChannel(),
+            chatMessage.Author.Identifier,
+            _timeProvider.GetCurrentUtcTime()));
+    }
+
+    public int GetMessageCountInChannel(
+        string channelId,
+        TimeSpan maxTimeAgo)
+    {
+        DateTimeOffset timeLimit = _timeProvider.GetCurrentUtcTime() - maxTimeAgo;
+        return _repository
+            .GetAll()
+            .Count(o => o.ChannelId == channelId && o.OccurredAt >= timeLimit);
+    }
+}
+
