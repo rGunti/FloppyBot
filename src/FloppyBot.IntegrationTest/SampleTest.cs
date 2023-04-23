716c8c4c400ff5f3529a7288da6aa061acd966d7
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2023-04-23 13:00:50 +0200

    implemented redis testing

--- src/FloppyBot.IntegrationTest/SampleTest.cs
+++ src/FloppyBot.IntegrationTest/SampleTest.cs
@@ -1,24 +1,22 @@
-using DotNet.Testcontainers.Containers;
 using FloppyBot.Base.Storage.MongoDb;
+using FloppyBot.Communication.Redis;
 using FloppyBot.FileStorage.Entities;
-using MongoDB.Driver;
-using Testcontainers.MongoDb;
+using FloppyBot.IntegrationTest.Setup;
 
 namespace FloppyBot.IntegrationTest;
 
 public class SampleTest : IAsyncLifetime
 {
-    private readonly IContainer _container;
-    private IMongoClient _mongoClient = null!;
+    private readonly TestContainerSetup _containers;
     private MongoDbRepositoryFactory _repositoryFactory = null!;
 
     public SampleTest()
     {
-        _container = new MongoDbBuilder().WithPortBinding(27017, true).Build();
+        _containers = new TestContainerSetup();
     }
 
     [Fact]
-    public void RunTest()
+    public void TestMongoDatabase()
     {
         var repository = _repositoryFactory.GetRepository<FileHeader>();
         var fileHeader = new FileHeader(null!, "Panda", "Sample File", 25000, "text/plain");
@@ -29,19 +27,47 @@ public class SampleTest : IAsyncLifetime
         Assert.Single(fileHeaders, h => h == fileHeader with { Id = null!, });
     }
 
+    [Fact]
+    public void TestRedis()
+    {
+        var sender = new RedisNotificationSender(
+            _containers.ConnectionMultiplexer.GetSubscriber(),
+            "Sample.Channel"
+        );
+        var receiver = new RedisNotificationReceiver<FileHeader>(
+            _containers.ConnectionMultiplexer.GetSubscriber(),
+            "Sample.Channel"
+        );
+
+        var sampleHeader = new FileHeader("sampleHeader", "Panda", "Some Name", 240, "text/plain");
+
+        var receivedHeaders = new List<FileHeader>();
+        receiver.NotificationReceived += (header) =>
+        {
+            receivedHeaders.Add(header);
+        };
+        receiver.StartListening();
+
+        sender.Send(sampleHeader);
+
+        Thread.Sleep(2000);
+
+        receiver.StopListening();
+
+        Assert.Single(receivedHeaders);
+        Assert.Equal(sampleHeader, receivedHeaders.First());
+    }
+
     public async Task InitializeAsync()
     {
-        await _container.StartAsync();
-
-        var mongoUrl = MongoUrl.Create(
-            $"mongodb://mongo:mongo@localhost:{_container.GetMappedPublicPort(27017)}"
+        await _containers.Startup();
+        _repositoryFactory = new MongoDbRepositoryFactory(
+            _containers.MongoClient.GetDatabase("test")
         );
-        _mongoClient = new MongoClient(mongoUrl);
-        _repositoryFactory = new MongoDbRepositoryFactory(_mongoClient.GetDatabase("test"));
     }
 
     public async Task DisposeAsync()
     {
-        await _container.StopAsync();
+        await _containers.Shutdown();
     }
 }
---
+++ src/FloppyBot.IntegrationTest/Setup/TestContainerSetup.cs
@@ -0,0 +1,60 @@
+using DotNet.Testcontainers.Containers;
+using MongoDB.Driver;
+using StackExchange.Redis;
+using Testcontainers.MongoDb;
+using Testcontainers.Redis;
+
+namespace FloppyBot.IntegrationTest.Setup;
+
+public class TestContainerSetup : IAsyncDisposable
+{
+    private readonly Lazy<MongoClient> _mongoClient;
+    private readonly IContainer _mongoDbContainer;
+    private readonly Lazy<IConnectionMultiplexer> _redisConnection;
+    private readonly IContainer _redisContainer;
+
+    public TestContainerSetup()
+    {
+        _mongoDbContainer = new MongoDbBuilder().WithPortBinding(27017, true).Build();
+        _redisContainer = new RedisBuilder().WithPortBinding(6379, true).Build();
+
+        _mongoClient = new Lazy<MongoClient>(
+            () => new MongoClient(MongoUrl.Create(GetMongoConnectionString()))
+        );
+        _redisConnection = new Lazy<IConnectionMultiplexer>(
+            () => StackExchange.Redis.ConnectionMultiplexer.Connect(GetRedisConnectionString())
+        );
+    }
+
+    public IConnectionMultiplexer ConnectionMultiplexer => _redisConnection.Value;
+    public MongoClient MongoClient => _mongoClient.Value;
+
+    public async ValueTask DisposeAsync()
+    {
+        await _mongoDbContainer.DisposeAsync();
+        await _redisContainer.DisposeAsync();
+        GC.SuppressFinalize(this);
+    }
+
+    public async Task Startup()
+    {
+        await _mongoDbContainer.StartAsync();
+        await _redisContainer.StartAsync();
+    }
+
+    public async Task Shutdown()
+    {
+        await _mongoDbContainer.StopAsync();
+        await _redisContainer.StopAsync();
+    }
+
+    private string GetMongoConnectionString()
+    {
+        return $"mongodb://mongo:mongo@localhost:{_mongoDbContainer.GetMappedPublicPort(27017)}";
+    }
+
+    private string GetRedisConnectionString()
+    {
+        return $"localhost:{_redisContainer.GetMappedPublicPort(6379)}";
+    }
+}
