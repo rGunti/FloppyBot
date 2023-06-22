c20abf82919290d61038aa22ca22b9982f4c6b66
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2023-06-22 20:45:40 +0200

    updated build process

--- scripts/determine-version.sh
+++ scripts/determine-version.sh
@@ -1,16 +1,16 @@
 ﻿#!/bin/bash
-source /semver.sh
+source ./semver.sh
 
 MAJOR=0  
 MINOR=0  
 PATCH=0  
 SPECIAL=""
 
-semverParseInto "$(cat /app-version)" MAJOR MINOR PATCH SPECIAL
+semverParseInto "$(cat ./app-version)" MAJOR MINOR PATCH SPECIAL
 
 echo MAJOR=${MAJOR}
 echo MINOR=${MINOR}
 echo PATCH=${PATCH}
 echo SPECIAL=${SPECIAL}
 
-echo "${MAJOR}.${MINOR}.${PATCH}" > /app-short-version
+echo "${MAJOR}.${MINOR}.${PATCH}" > ./app-short-version
