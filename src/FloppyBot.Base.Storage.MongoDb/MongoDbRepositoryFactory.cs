1da9fd7bb3e04c6126320161326945228d97edb2
Raphael Guntersweiler <raphael+git@guntersweiler.net>
2024-05-09 15:24:08 +0200

    will now create mongodb collection

--- src/FloppyBot.Base.Storage.MongoDb/MongoDbRepositoryFactory.cs
+++ src/FloppyBot.Base.Storage.MongoDb/MongoDbRepositoryFactory.cs
@@ -1,15 +1,22 @@
 ﻿using FloppyBot.Base.Storage.Utils;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace FloppyBot.Base.Storage.MongoDb;
 
 public class MongoDbRepositoryFactory : IRepositoryFactory
 {
+    private readonly ILogger<MongoDbRepositoryFactory> _logger;
     private readonly IMongoDatabase _database;
 
-    public MongoDbRepositoryFactory(IMongoDatabase database)
+    public MongoDbRepositoryFactory(
+        IMongoDatabase database,
+        ILogger<MongoDbRepositoryFactory> logger
+    )
     {
         _database = database;
+        _logger = logger;
     }
 
     public IRepository<T> GetRepository<T>()
@@ -19,6 +26,20 @@ public class MongoDbRepositoryFactory : IRepositoryFactory
     public IRepository<T> GetRepository<T>(string collectionName)
         where T : class, IEntity<T>
     {
+        var collectionExists = _database
+            .ListCollections(
+                new ListCollectionsOptions
+                {
+                    Filter = Builders<BsonDocument>.Filter.Eq(f => f["name"], collectionName),
+                }
+            )
+            .Any();
+        if (!collectionExists)
+        {
+            _logger.LogDebug("Creating collection {CollectionName}", collectionName);
+            _database.CreateCollection(collectionName);
+        }
+
         return new MongoDbRepository<T>(_database.GetCollection<T>(collectionName));
     }
 }
